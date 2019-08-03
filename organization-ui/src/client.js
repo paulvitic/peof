61d7bb5ab8be8f442d458d2f44d5c8a89e3f3f4a
pvitic <paul.vitic@smava.de>
2019-08-03 19:58:24 +0200

    fail an eslint check

--- organization-ui/src/client.js
+++ organization-ui/src/client.js
@@ -1,5 +1,6 @@
 /* globals window */
 import CompanyList from './company/CompanyList';
 
+let unusedVariable;
 window.blue = { count: 0 };
 window.customElements.define('company-list', CompanyList);
