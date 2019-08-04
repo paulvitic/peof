12ca81e9c373513a85d356fa67bc91d8a708d826
pvitic <paul.vitic@smava.de>
2019-08-04 09:10:28 +0200

    rename documents folder as docs for github

--- organization-ui/src/client.js
+++ organization-ui/src/client.js
@@ -1,6 +1,5 @@
 /* globals window */
 import CompanyList from './company/CompanyList';
 
-let unusedVariable;
 window.blue = { count: 0 };
 window.customElements.define('company-list', CompanyList);
