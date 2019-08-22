375ba34b09443e9ea58131712b45b626912427b4
pvitic <paul.vitic@smava.de>
2019-08-22 17:15:07 +0200

    commit for #17. Created an example container service deployment CloudFormation template

--- .aws/bootstrap.sh
+++ .aws/bootstrap.sh
@@ -2,10 +2,24 @@
 
 
 #aws --debug cloudformation deploy  \
-#    --template ./cluster/cluster-ec2-private-vpc.yml \
-#    --stack-name peof-cluster \
+#    --template ./cluster/cluster-ec2-public-vpc.yml \
+#    --stack-name peof-public-cluster \
 #    --capabilities CAPABILITY_IAM \
-#    --parameter-overrides EnvironmentName=peof-prod
+#    --parameter-overrides EnvironmentName=peof-public-prod
 
-aws --debug cloudformation delete-stack \
-    --stack-name peof-network
+#aws --debug cloudformation deploy  \
+#    --template ./ingress/alb-external.yml \
+#    --stack-name peof-ext-ingress \
+#    --capabilities CAPABILITY_IAM \
+#    --parameter-overrides EnvironmentName=peof-public-prod
+
+aws --debug cloudformation deploy  \
+    --template ./service/portainer-ec2-public-lb.yml \
+    --stack-name peof-portainer \
+    --parameter-overrides EnvironmentName=peof-public-prod
+
+#aws --debug cloudformation delete-stack \
+#    --stack-name peof-portainer
+
+#aws --debug cloudformation delete-stack \
+#    --stack-name peof-network
