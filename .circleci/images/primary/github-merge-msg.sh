2985633145f23f2ef69028456715d6319d931534
pvitic <paul.vitic@smava.de>
2019-08-27 17:35:21 +0200

    #23 test deploy to stage

--- .circleci/images/primary/github-merge-msg.sh
+++ .circleci/images/primary/github-merge-msg.sh
@@ -4,4 +4,10 @@ export GITHUB_PR_USERNAME=$(git log -1 --pretty=%B | grep -Po '([a-z]|\d|-)*\/' 
 export GITHUB_PR_SOURCE_ROOT=$(git log -1 --pretty=%B | grep -Po '([a-z]|\d|-)*\/' | sed "s,/$,," | awk 'NR==2')
 export GITHUB_PR_ISSUE=$(git log -1 --pretty=%B | grep -Po '([a-z]|\d|-)*\/' | sed "s,/$,," | awk 'NR==3')
 
+# TODO do some validation
+
 echo "Pull request was for '${GITHUB_PR_ISSUE}' for sub source directory '${GITHUB_PR_SOURCE_ROOT}' merged by '${GITHUB_PR_USERNAME}'."
+
+export GITHUB_PR_SOURCE_ROOT=$GITHUB_PR_SOURCE_ROOT
+export GITHUB_PR_USERNAME=$GITHUB_PR_USERNAME
+export GITHUB_PR_ISSUE=$GITHUB_PR_ISSUE
