159d9fd0b2d8e0f1d2cc4b55bef965124b26f7b4
pvitic <paul.vitic@smava.de>
2019-08-29 11:04:46 +0200

    #48 not related to ticket but changed start script for organization-ui, externalized NODE_ENV variable and moved configuration properties to json file

--- organization-ui/src/config.js
+++ organization-ui/src/config.js
@@ -1,62 +1,7 @@
-const env = process.env.NODE_ENV;
+import * as config from './properties.json';
 
+// If NODE_ENV environment variable is not set use dev
+const env = process.env.NODE_ENV || "dev";
 console.log('[info] running in ' + env + ' mode.' );
-
-const dev = {
-  app: {
-    port: 3002
-  },
-  db: {
-    host: '0.0.0.0',
-    port: 27017,
-    name: 'orgView',
-    user: 'orgView',
-    password: 'password'
-  },
-  queue: {
-    host: '0.0.0.0',
-    port: '5672'
-  }
-};
-
-const test = {
-  app: {
-    port: 3002
-  },
-  db: {
-    host: 'localhost',
-    port: 27017,
-    name: 'test',
-    user: 'orgView',
-    password: 'password'
-  },
-  queue: {
-    host: '0.0.0.0',
-    port: '5672'
-  }
-};
-
-const prod = {
-  app: {
-    port: 3002
-  },
-  db: {
-    host: 'peof_mongo_1',
-    port: 27017,
-    name: 'orgView',
-    user: 'orgView',
-    password: 'password'
-  },
-  queue: {
-    host: 'peof_rabbitmq_1',
-    port: '5672'
-  }
-};
-
-const config = {
-  dev,
-  test,
-  prod
-};
 
 export default config[env];
