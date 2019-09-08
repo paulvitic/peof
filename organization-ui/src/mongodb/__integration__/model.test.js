1976d57c904c5522c6ab656c5141b5afb8f9f9e8
pvitic <paul.vitic@smava.de>
2019-09-08 09:37:46 +0200

    #50 make eslint pass again

--- organization-ui/src/mongodb/__integration__/model.test.js
+++ organization-ui/src/mongodb/__integration__/model.test.js
@@ -2,50 +2,50 @@ import mongoose from 'mongoose';
 import { InitMongoDb, CreateCompany } from '../model';
 
 async function removeAllCollections () {
-    const collections = Object.keys(mongoose.connection.collections);
-    for (const collectionName of collections) {
-        const collection = mongoose.connection.collections[collectionName];
-        await collection.deleteMany()
-    }
+  const collections = Object.keys(mongoose.connection.collections);
+  for (const collectionName of collections) {
+    const collection = mongoose.connection.collections[collectionName];
+    await collection.deleteMany();
+  }
 }
 
 async function dropAllCollections () {
-    const collections = Object.keys(mongoose.connection.collections);
-    for (const collectionName of collections) {
-        const collection = mongoose.connection.collections[collectionName];
-        try {
-            await collection.drop()
-        } catch (error) {
-            // Sometimes this error happens, but you can safely ignore it
-            if (error.message === 'ns not found') return;
-            // This error occurs when you use it.todo. You can
-            // safely ignore this error too
-            if (error.message.includes('a background operation is currently running')) return
-            console.log(error.message)
-        }
+  const collections = Object.keys(mongoose.connection.collections);
+  for (const collectionName of collections) {
+    const collection = mongoose.connection.collections[collectionName];
+    try {
+      await collection.drop();
+    } catch (error) {
+      // Sometimes this error happens, but you can safely ignore it
+      if (error.message === 'ns not found') return;
+      // This error occurs when you use it.todo. You can
+      // safely ignore this error too
+      if (error.message.includes('a background operation is currently running')) return;
+      console.log(error.message);
     }
+  }
 }
 
-describe("Company operations", function() {
+describe('Company operations', function() {
 
-    beforeAll(async () => {
-        await InitMongoDb();
-    });
+  beforeAll(async () => {
+    await InitMongoDb();
+  });
 
-    afterEach(async () => {
-        await removeAllCollections();
-    });
+  afterEach(async () => {
+    await removeAllCollections();
+  });
 
-    afterAll(async () => {
-        await dropAllCollections();
-        await mongoose.connection.close();
-    });
+  afterAll(async () => {
+    await dropAllCollections();
+    await mongoose.connection.close();
+  });
 
-    it('Should save Company', async done => {
-        const mockAck = jest.fn();
-        CreateCompany({companyName: "Test Company", aggregateId: "12345"}, mockAck);
-        console.log("[info] Mock called with argument: " + mockAck.mock.calls);
-        expect(mockAck).toHaveBeenCalled();
-        done();
-    });
+  it('Should save Company', async done => {
+    const mockAck = jest.fn();
+    CreateCompany({companyName: 'Test Company', aggregateId: '12345'}, mockAck);
+    console.log('[info] Mock called with argument: ' + mockAck.mock.calls);
+    expect(mockAck).toHaveBeenCalled();
+    done();
+  });
 });
