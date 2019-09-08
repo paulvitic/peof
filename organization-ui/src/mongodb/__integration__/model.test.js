6bd359a498237c46f34275a610f28aec0ddb5e4b
pvitic <paul.vitic@smava.de>
2019-09-08 09:47:24 +0200

    #50 initialize secondary container mongo

--- organization-ui/src/mongodb/__integration__/model.test.js
+++ organization-ui/src/mongodb/__integration__/model.test.js
@@ -18,8 +18,7 @@ async function dropAllCollections () {
     } catch (error) {
       // Sometimes this error happens, but you can safely ignore it
       if (error.message === 'ns not found') return;
-      // This error occurs when you use it.todo. You can
-      // safely ignore this error too
+      // This error occurs when you use it. You can safely ignore this error too
       if (error.message.includes('a background operation is currently running')) return;
       console.log(error.message);
     }
