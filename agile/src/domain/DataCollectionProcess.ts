ebd4b5c846db13a70905475a7fb6811f69693f49
paulvitic <git1212Paul>
2019-11-17 19:34:57 +0100

    #52 initial set up

--- agile/src/domain/DataCollectionProcess.ts
+++ agile/src/domain/DataCollectionProcess.ts
@@ -1,6 +1,6 @@
 /**
 curl --request GET \
-  --url 'https://comatch.atlassian.net/rest/api/3/search?jql=status%20not%20in%20(Closed%2C%20Done)%20and%20project%3DContact&maxResults=1&fields=statuscategorychangedate,issuetype,project' \
-  --user 'p.vitic@comatch.com:amyl2uWaTymL0oEm0dcHA6F4' \
+  --url 'https://some.atlassian.net/rest/api/3/search?jql=status%20not%20in%20(Closed%2C%20Done)%20and%20project%3DContact&maxResults=1&fields=statuscategorychangedate,issuetype,project' \
+  --user 'email@company.com:amyl2uWaTymL0oEm0dcHA6F4' \
   --header 'Accept: application/json'
  **/
