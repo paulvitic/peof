ccfec77cb1569063f6bdf621ec9aadd8e4107ac1
paulvitic <git1212Paul>
2019-11-30 16:03:42 +0100

    #52 test

--- agile/test/domain/Except.test.ts
+++ agile/test/domain/Except.test.ts
@@ -1,4 +1,4 @@
-import {Fail, Failure, withFailure, withSuccess} from "../../src/domain/Except";
+import {withFailure, withSuccess} from "../../src/domain/Except";
 
 test('should get failure value', () => {
     const failure = {reason: "A failure.", type: 0};
@@ -9,5 +9,27 @@ test('should get failure value', () => {
 test('should get success value', () => {
     const except = withSuccess(1);
     expect(except.value).toBe(1);
+});
+
+test('should invoke on success and return another expect', () => {
+    const onSuccess = jest.fn(x => 1 + x);
+    let except = withSuccess(1).onSuccess(onSuccess);
+    expect(onSuccess).toBeCalled();
+    expect(except.value).toBe(2);
+});
+
+test('should invoke on success and return another failed expect', () => {
+    const failure = {reason: "A failure.", type: 0};
+    const onSuccess = jest.fn(() => withFailure(failure));
+    let except = withSuccess(1).onSuccess(onSuccess);
+    expect(onSuccess).toBeCalled();
+    // @ts-ignore
+    expect(except.value.value).toBe(failure);
+});
+
+test('should not call else on success', () => {
+    const onFail = jest.fn(() => {});
+    const except = withSuccess(1).else(onFail);
+    expect(onFail).not.toBeCalled();
 });
 
